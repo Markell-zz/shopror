4f9845a6cd01d0a1933768adf63066b90b79146d
Markell <mark.s15@yandex.ru>
2014-03-21 20:35:37 +0400

    new validates

--- my_store/app/models/item.rb
+++ my_store/app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
 	validates :price, numericality: { greater_than: 0, allow_nil: true } 
-    #validates :name, :description, presense: true	
+    validates :name, :description, presence: true	
 end
