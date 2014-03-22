33d8f77fc019fda5493ca3dd517dfe477603109c
Markell <mark.s15@yandex.ru>
2014-03-22 19:24:17 +0400

    new callback

--- my_store/app/models/item.rb
+++ my_store/app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
 	validates :price, numericality: { greater_than: 0, allow_nil: true } 
     validates :name, :description, presence: true	
+    
+    belongs_to :category
+
+    after_initialize {  } # Item.new Item.first
+    after_save       {  }      # Item.save; Item.create; item.update_atributes()
+    after_create     { category.inc(:items_count, 1) }
+    after_update     {  }
+    after_destroy    { category.inc(:items_count, -1) }  # Item.destroy
 end
