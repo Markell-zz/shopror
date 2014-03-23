fed50006f8002748b81fcd31f7aa0cfea6128fd9
Markell <mark.s15@yandex.ru>
2014-03-23 21:19:48 +0400

    RESTful controllers

--- my_store/app/controllers/items_controller.rb
+++ my_store/app/controllers/items_controller.rb
@@ -1,15 +1,43 @@
 class ItemsController < ApplicationController
 
+
 def index
  @items = Item.all 
  render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
 end
 
-def create
 
- @item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
+# /items/1 GET
+def show
+end
+
+
+#/items/new GET
+def new
+end
+
+
+#/items/1/edit GET
+def edit
+end
+
+
+#/items POST
+def create
+     
+    render text: "item create!"
+    #@item = Item.create(name: params[:name], description: params[:description], price: params[:price], real: params[:real], weight: params[:weight])
  
-	render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+end
+
+
+#/items/1 PUT
+def update	
+end
+
+
+#/items/1 DELETE
+def destroy	
 end
 
 end
